4fd90eaaa09027a51592912cf7e1e96a85b3fb0e
e2y <Esythara@outlook.com>
2022-03-03 17:34:02 +0000

    wr
    wr - will retract
    
    added an modern way of pushing error messages, however, need to fix the current error message(s) received

--- includes/article.php
+++ includes/article.php
@@ -29,3 +29,37 @@ function getArticle($conn, $id)
     }
   }
 }
+
+
+/**
+  *validating the article properties
+  *param string $title title, required
+  *param string $content content, required
+  *param string $published_at published at, required
+  *
+  *return array an array of validation error messages
+  */
+
+
+function validateArticle($title, $content, $published_at)
+{
+  $errors = [];
+  $title = '';
+  $content = '';
+  $published_at = '';
+
+  $required = Array("title", "content");
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $published_at = $_POST['published_at'];
+
+  foreach($required as $requiredfield) {
+    if($_POST[$requiredfield] == '') {
+      $errors[] = $requiredfield . ' required';
+    }
+  }
+  return $errors;
+}
