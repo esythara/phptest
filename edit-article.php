4eef8a726f73029b6ac43bf589b0c568f2fd3b42
e2y <Esythara@outlook.com>
2022-03-03 17:18:43 +0000

    edit form
    Provided error message for the edit-article form

--- edit-article.php
+++ edit-article.php
@@ -9,8 +9,24 @@ if (isset($_GET['id'])) {
 
   $article = getArticle($conn, $_GET['id']);
 
+  if ($article) {
+
+    $title = $article['title'];
+    $content = $article['content'];
+    $published_at = $article['published_at'];
+  } else {
+    die("mom is not here");
+  }
+
 } else {
-  $article = null;
+  die("id not supplied, ur mom isn't here");
 }
 
-var_dump($article);
+?>
+<?php require 'includes/header.php'; ?>
+
+<h2>edit ur mom</h2>
+
+<?php require 'includes/article-form.php'; ?>
+
+<?php require 'includes/footer.php'; ?>
