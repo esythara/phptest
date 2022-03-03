1f9edd93fe62b6aa3ec56bdff53d655dc63d4990
e2y <Esythara@outlook.com>
2022-03-03 17:50:29 +0000

    Error messages
    adapted the edit script to provide intel to whether or not the data is valid

--- edit-article.php
+++ edit-article.php
@@ -22,6 +22,18 @@ if (isset($_GET['id'])) {
   die("id not supplied, ur mom isn't here");
 }
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $title = $_POST['title'];
+  $content = $_POST['content'];
+  $published_at = $_POST['published_at'];
+
+  $errors = validateArticle($title, $content, $published_at);
+
+  if (empty($errors)) {
+    die("this is a valid mother");
+  }
+}
+
 ?>
 <?php require 'includes/header.php'; ?>
 
