72c1d850acee59c561d134c168a04924675358ea
e2y <Esythara@outlook.com>
2022-03-03 17:11:42 +0000

    spring cleaning
    Tidied up the code

--- new-article.php
+++ new-article.php
@@ -78,33 +78,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <h2>New mom</h2>
 
-<?php if (! empty($errors)): ?>
-  <ul>
-    <?php foreach ($errors as $error): ?>
-      <li><?= $error ?></li>
-    <?php endforeach; ?>
-  </ul>
-<?php endif; ?>
-
-<form method="post">
-
-  <div>
-    <label for="title">title</label>
-    <input name="title" id="title" placeholder="yr mom" value="<?= htmlspecialchars($title); ?>">
-  </div>
-
-  <div>
-    <label for="content">content</label>
-    <textarea name="content" rows="4" cols="40" id="content" placeholder="content"><?= $content; ?></textarea>
-  </div>
-
-  <div>
-    <label for="published_at">when it were posted init</label>
-    <input type="datetime-local" name="published_at" id="published_at" value="<?= $published_at; ?>">
-  </div>
-
-  <button>add</button>
-
-</form>
+<?php require 'includes/article-form.php'; ?>
 
 <?php require 'includes/footer.php'; ?>
